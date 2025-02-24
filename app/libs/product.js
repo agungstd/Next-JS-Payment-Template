c45ce22f3f5c9689b6b4d30f9d45fc2d399061f9
agungstd <agungpriambodo@gmail.com>
2025-02-24 14:33:36 +0700

    Added additional colors to the product file library

--- app/libs/product.js
+++ app/libs/product.js
@@ -1,13 +1,18 @@
 export const product = {
-    id: ~~(Math.random() * 100) + 1,
-    image: "/img.png",
-    name: "LEVI'S® WOMEN'S XL TRUCKER JACKET",
-    price: 350000,
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusantium, aspernatur provident beatae corporis veniam atque facilis, consequuntur assumenda, vitae dignissimos iste exercitationem dolor eveniet alias eos ullam nesciunt voluptatum",
-    colors: [
-        { value: "putih", label: "Putih" },
-        { value: "biru", label: "Biru" },
-        { value: "coklat", label: "Coklat" },
-        { value: "kuning", label: "Kuning" }
-    ]
-}
+  id: ~~(Math.random() * 100) + 1,
+  image: "/img.png",
+  name: "LEVI'S® WOMEN'S XL TRUCKER JACKET",
+  price: 350000,
+  description:
+    "Jaket denim klasik dari LEVI'S® yang didesain khusus untuk wanita dengan ukuran XL. Terbuat dari bahan berkualitas tinggi, memberikan kenyamanan dan gaya yang tak tertandingi.",
+  colors: [
+    { value: "putih", label: "Putih" },
+    { value: "biru", label: "Biru" },
+    { value: "coklat", label: "Coklat" },
+    { value: "kuning", label: "Kuning" },
+    { value: "merah", label: "Merah" },
+    { value: "hijau", label: "Hijau" },
+    { value: "hitam", label: "Hitam" },
+    { value: "abu-abu", label: "Abu-Abu" },
+  ],
+};
